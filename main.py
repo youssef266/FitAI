74f960775604c5d054c96f4988e4e4ff54bf2648
youssef266 <youssef.toti.taha.1212@gmail.com>
2026-03-04 22:07:23 +0200

    remove the secret key to make the repo public

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def AI_service():
     userid = session['user_id']
     user = mysession.query(Users).filter_by(id=userid).first()
 
-    client = Groq(api_key="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")
+    client = Groq(api_key="secret-key")
     chat_completion = client.chat.completions.create(
         messages=[
             {"role": "user", "content": prompt}
